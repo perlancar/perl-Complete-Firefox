8690b4bb0c7498dcf5d474aef9e489cf5e9f10b9
perlancar (on pc-home) <perlancar@gmail.com>
2020-04-18 07:30:04 +0700

    Initial commit

---
+++ lib/Complete/Firefox.pm
@@ -0,0 +1,59 @@
+package Complete::Firefox;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+require Exporter 'import';
+our @EXPORT_OK = qw(
+                       complete_firefox_profile_name
+               );
+
+our %SPEC;
+
+$SPEC{':package'} = {
+    v => 1.1,
+    summary => 'Completion routines related to Firefox',
+};
+
+$SPEC{complete_firefox_profile_name} = {
+    v => 1.1,
+    args => {
+        word => {
+            schema => 'str*',
+            req => 1,
+            pos => 0,
+        },
+    },
+};
+sub complete_firefox_profile_name {
+    require Complete::Util;
+    require Firefox::Util::Profile;
+
+    my %args = @_;
+
+    my $res = Firefox::Util::Profile::list_firefox_profiles(detail=>1);
+    return {message=>"Can't list Firefox profiles: $res->[0] - $res->[1]"}
+        unless $res->[0] == 200;
+
+    Complete::Util::complete_array_elem(
+        word  => $args{word},
+        array => [map {$_->{name}} @{ $res->[2] }],
+    );
+}
+
+1;
+# ABSTRACT:
+
+=for Pod::Coverage .+
+
+=head1 SEE ALSO
+
+L<Complete>
+
+Other C<Complete::*> modules.
