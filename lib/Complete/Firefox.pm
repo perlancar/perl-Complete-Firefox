5f8b75caa971b4ca3b490b0d7c1fae98d445fb63
perlancar (on pc-home) <perlancar@gmail.com>
2021-05-09 19:10:12 +0700

    [doc] Add Rinci summary to function

--- lib/Complete/Firefox.pm
+++ lib/Complete/Firefox.pm
@@ -23,6 +23,7 @@ $SPEC{':package'} = {
 
 $SPEC{complete_firefox_profile_name} = {
     v => 1.1,
+    summary => 'Complete from a list of Firefox profile names',
     args => {
         word => {
             schema => 'str*',
